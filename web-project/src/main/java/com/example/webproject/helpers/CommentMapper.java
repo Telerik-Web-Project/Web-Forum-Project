bb07b5e994e283e3c497860b63b8e9440537bbdd
Stoyan Dimitrov <agilitymuffin@gmail.com>
2023-10-16 23:57:03 +0300

    commit CommentMapper

---
+++ web-project/src/main/java/com/example/webproject/helpers/CommentMapper.java
@@ -0,0 +1,37 @@
+package com.example.webproject.helpers;
+
+import com.example.webproject.models.Comment;
+import com.example.webproject.models.CommentDto;
+import com.example.webproject.repositories.CommentRepository;
+import com.example.webproject.repositories.PostRepository;
+import com.example.webproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentMapper {
+    private CommentRepository commentRepository;
+    @Autowired
+    public CommentMapper(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Comment fromCommentDto(int id, CommentDto dto) {
+        Comment comment = fromDto(dto); // new Comment with content == content of DTO
+        comment.setId(id);// setting the new comment id with the id of the @pathvariable
+        Comment repositoryComment = commentRepository.get(id);// retrieving the existing comment form repo
+        comment.setUser(repositoryComment.getUser());// setting new comment the same creator as the comment from repo
+        comment.setPost(repositoryComment.getPost());// setting new comment the same post it belongs to as the
+        // comment from repo
+        return comment;
+    }
+
+    public Comment fromDto(CommentDto dto) {
+        Comment comment = new Comment();
+        comment.setContent(dto.getContent());
+        return comment;
+    }
+
+
+
+}
