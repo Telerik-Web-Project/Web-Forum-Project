7503fbbfe373a82835bab80198be0bf6fc5e7a0e
Stoyan Dimitrov <agilitymuffin@gmail.com>
2023-10-16 23:54:43 +0300

    commit CommentDto

---
+++ web-project/src/main/java/com/example/webproject/models/CommentDto.java
@@ -0,0 +1,21 @@
+package com.example.webproject.models;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CommentDto {
+    @NotNull
+    @Size(min = 1, max = 250, message = "Comments can be no more than 250 symbols.")
+    private String content;
+
+    public CommentDto() {
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
